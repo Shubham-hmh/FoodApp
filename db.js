eae60358821f37900f066664c22e8cf8a0dab500
shubham-hmh <shubham_203054@saitm.org>
2023-01-10 17:48:12 +0530

    home

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
 const mongoose = require('mongoose');
-//  const mongoURI = "mongodb+srv://goFood:goFood123@cluster0.uqyrima.mongodb.net/goFoodMern?retryWrites=true&w=majority"
 
  const dotenv=require("dotenv");
  dotenv.config();
-// const mongoDBconnect = () => {
 
-//     mongoose.connect(mongoURI).then(() => {
-//         console.log("db connection successful");
-//     }).catch((e) => {
-//         console.log("db connection failed");
-//     })
-// }
 const mongoDBconnect = async () => {
 
     await mongoose.connect(process.env.mongoURI, { useNewUrlParser: true }, async (err, result) => {
